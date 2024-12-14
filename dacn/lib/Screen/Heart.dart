667985bfe46dc154a17e6134fe24a4e3aaa0c26e
DoanVanDu <125144986+DoanVanDu-2180606750@users.noreply.github.com>
2024-12-14 19:52:47 +0700

    heart

--- dacn/lib/Screen/Heart.dart
+++ dacn/lib/Screen/Heart.dart
@@ -3,7 +3,7 @@ import 'package:heart_bpm/chart.dart';
 import 'package:heart_bpm/heart_bpm.dart';
 
 class HeartRateScreen extends StatefulWidget {
-  const HeartRateScreen({Key? key}) : super(key: key);
+  const HeartRateScreen({super.key});
 
   @override
   _HeartRateScreenState createState() => _HeartRateScreenState();
@@ -21,97 +21,102 @@ class _HeartRateScreenState extends State<HeartRateScreen> {
       backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 255, 82, 243),
-        title: Text('Đo nhịp tim', style: TextStyle(color: Colors.white)),
+        title: const Text('Đo nhịp tim', style: TextStyle(color: Colors.white)),
         centerTitle: true,
         elevation: 0,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              if (isBPMEnabled)
-                bpmDialog = HeartBPMDialog(
-                  context: context,
-                  showTextValues: true,
-                  borderRadius: 10,
-                  onRawData: (value) {
-                    setState(() {
-                      if (data.length >= 100) data.removeAt(0);
-                      data.add(value);
-                    });
-                  },
-                  onBPM: (value) => setState(() {
-                    if (bpmValues.length >= 100) bpmValues.removeAt(0);
-                    bpmValues.add(SensorValue(
-                        value: value.toDouble(), time: DateTime.now()));
-                  }),
-                ),
-              SizedBox(height: 20),
-              if (isBPMEnabled && data.isNotEmpty)
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                if (isBPMEnabled)
+                  bpmDialog = HeartBPMDialog(
+                    context: context,
+                    showTextValues: true,
+                    borderRadius: 10,
+                    onRawData: (value) {
+                      setState(() {
+                        // Ensure we keep only the latest 100 readings
+                        if (data.length >= 100) data.removeAt(0);
+                        data.add(value);
+                      });
+                    },
+                    onBPM: (value) {
+                      if (value >= 30 && value <= 200) { // Validate BPM values
+                        setState(() {
+                          // Keep the last 100 BPM readings
+                          if (bpmValues.length >= 100) bpmValues.removeAt(0);
+                          bpmValues.add(SensorValue(value: value.toDouble(), time: DateTime.now()));
+                        });
+                      } else {
+                        // Optionally, show an error message or log this data point
+                        print("Invalid BPM value: $value");
+                      }
+                    },
                   ),
-                  height: 180,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: BPMChart(data),
-                  ),
-                ),
-              SizedBox(height: 20),
-              if (isBPMEnabled && bpmValues.isNotEmpty)
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  height: 180,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: BPMChart(bpmValues),
-                  ),
-                ),
-              SizedBox(height: 40),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.favorite_rounded, color: Colors.white),
-                    label: Text(isBPMEnabled ? "Dừng đo" : "Đo nhịp tim"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 243, 82, 255),
-                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                      textStyle: TextStyle(fontSize: 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    onPressed: () => setState(() {
-                      isBPMEnabled = !isBPMEnabled;
-                    }),
-                  ),
-                ],
-              ),
-            ],
+                const SizedBox(height: 20),
+                if (isBPMEnabled && data.isNotEmpty)
+                  _buildChartContainer(data, "Raw Data Chart"),
+                const SizedBox(height: 20),
+                if (isBPMEnabled && bpmValues.isNotEmpty)
+                  _buildChartContainer(bpmValues, "BPM Chart"),
+                const SizedBox(height: 40),
+                _buildActionButton(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  // Builds the chart container
+  Widget _buildChartContainer(List<SensorValue> data, String title) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      height: 180,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            Expanded(
+              child: BPMChart(data),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Action button to start/stop BPM monitoring
+  Widget _buildActionButton() {
+    return ElevatedButton.icon(
+      icon: const Icon(Icons.favorite_rounded, color: Colors.white),
+      label: Text(isBPMEnabled ? "Dừng đo" : "Đo nhịp tim"),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 243, 82, 255),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        textStyle: const TextStyle(fontSize: 18),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      onPressed: () => setState(() {
+        isBPMEnabled = !isBPMEnabled; // Toggle the BPM measurement
+      }),
     );
   }
 }
